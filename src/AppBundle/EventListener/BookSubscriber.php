af11638f31073006db0ef2f634ee28217ad7e5cd
iassasin <iassasin@yandex.ru>
2017-10-26 14:44:23 +0300

    add doctrine subscriber, autodeleter and autoloader of files

---
+++ src/AppBundle/EventListener/BookSubscriber.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Doctrine\ORM\Events;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Filesystem\Filesystem;
+use AppBundle\Entity\Book;
+use AppBundle\Service\FileStorage;
+
+class BookSubscriber implements EventSubscriber
+{
+    private $storage;
+    private $filesystem;
+
+    public function __construct(FileStorage $storage)
+    {
+        $this->storage = $storage;
+        $this->filesystem = new Filesystem();
+    }
+
+    public function getSubscribedEvents()
+    {
+        return [
+            Events::postLoad,
+            Events::postPersist,
+            Events::postRemove,
+            Events::postUpdate,
+        ];
+    }
+
+    public function postLoad(LifecycleEventArgs $args)
+    {
+        $this->loadFiles($args);
+    }
+
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $this->loadFiles($args);
+    }
+
+    public function postRemove(LifecycleEventArgs $args)
+    {
+        $this->removeFiles($args);
+    }
+
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $this->loadFiles($args);
+    }
+
+    public function loadFiles(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+
+        if ($entity instanceof Book) {
+            if ($entity->getCoverPath() != '') {
+                $path = $this->storage->getFilePathByFileName($entity->getCoverPath());
+                if ($this->filesystem->exists($path)) {
+                    $entity->setCoverFile(new File($path));
+                }
+            }
+
+            if ($entity->getBookPath() != '') {
+                $path = $this->storage->getFilePathByFileName($entity->getBookPath());
+                if ($this->filesystem->exists($path)) {
+                    $entity->setBookFile(new File($path));
+                }
+            }
+        }
+    }
+
+    public function removeFiles(LifecycleEventArgs $args)
+    {
+        $entity = $args->getObject();
+
+        if ($entity instanceof Book) {
+            if ($entity->getCoverPath() != '') {
+                $this->storage->remove($entity->getCoverPath());
+            }
+
+            if ($entity->getBookPath() != '') {
+                $this->storage->remove($entity->getBookPath());
+            }
+        }
+    }
+}
--- src/AppBundle/Service/FileStorage.php
+++ src/AppBundle/Service/FileStorage.php
@@ -8,25 +8,27 @@ use Symfony\Component\Filesystem\Filesystem;
 class FileStorage
 {
     private $targetDir;
+    private $filesystem;
 
-    public function __construct($targetDir) {
+    public function __construct($targetDir)
+    {
         $this->targetDir = $targetDir;
+        $this->filesystem = new Filesystem();
     }
 
     public function upload(UploadedFile $file)
     {
-        $fs = new Filesystem();
         $baseDir = $this->getTargetDir();
 
         $fileName = sha1(uniqid()).'.'.($file->guessExtension() ?? '.bin');
         $f1 = '/'.$fileName[0];
         $f2 = '/'.$fileName[1];
 
-        if (!$fs->exists($baseDir.$f1)) {
-            $fs->mkdir($baseDir.$f1);
+        if (!$this->filesystem->exists($baseDir.$f1)) {
+            $this->filesystem->mkdir($baseDir.$f1);
         }
-        if (!$fs->exists($baseDir.$f1.$f2)) {
-            $fs->mkdir($baseDir.$f1.$f2);
+        if (!$this->filesystem->exists($baseDir.$f1.$f2)) {
+            $this->filesystem->mkdir($baseDir.$f1.$f2);
         }
 
         $file->move($baseDir.$f1.$f2, $fileName);
@@ -34,7 +36,16 @@ class FileStorage
         return $fileName;
     }
 
-    public function getFilePathByFileName($fileName) {
+    public function remove($fileName)
+    {
+        $path = $this->getFilePathByFileName($fileName);
+        if ($this->filesystem->exists($path)) {
+            $this->filesystem->remove($path);
+        }
+    }
+
+    public function getFilePathByFileName($fileName)
+    {
         return $this->getTargetDir().'/'.$fileName[0].'/'.$fileName[1].'/'.$fileName;
     }
 
